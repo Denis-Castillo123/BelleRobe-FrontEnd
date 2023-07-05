d303cd719b81197c31917a83cd0a3895c7623eda
Denis-Castillo123 <90869371+Denis-Castillo123@users.noreply.github.com>
2023-07-05 16:55:21 -0600

    Integracion Vistas Componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { OrderComponent } from './pages/order/order.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'cart',
+    component: CartComponent
+  },
+  {
+    path: 'cart/continue',
+    component: OrderComponent
+  },
+  {
+    path: '',
+    redirectTo: "home",
+    pathMatch: "full"
+  },
+  { path: '404', 
+  loadChildren: 
+    () => import('./components/not-found/not-found.module')
+      .then(m => m.NotFoundModule) 
+  },
+  { 
+    path: 'auth', 
+    loadChildren: 
+    () => import('./authentication/authentication.module')
+    .then(m => m.AuthenticationModule) 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

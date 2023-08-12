fc7b00ba3df7717701773e67b435452e55cceb41
Denis-Castillo123 <90869371+Denis-Castillo123@users.noreply.github.com>
2023-08-11 19:22:59 -0600

    validaciones de maxlength - corrección del header por usuario

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -23,7 +23,8 @@ export class HeaderComponent implements OnInit {
 
   public logout() {
     this.userAuthService.clear();
-    this.router.navigate(['/']);
+    this.router.navigate(['/login']);
+    window.location.reload();
   }
 
   public isAdmin(){
